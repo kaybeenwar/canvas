25c44ffb5211e07cf3422de7d865c4f3f5fbd93b
Brayan Kabore <brayan.kabore@gmail.com>
2025-11-28 21:20:17 -0600

    Gpt help

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 plugins: [react()],
-base: '/cs3870_frontend/' // repo name between slashes
+base: '/canvas/' // repo name between slashes
 })
