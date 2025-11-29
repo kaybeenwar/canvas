4770fc38a3cc3fced022c279c34cfbf31ca500fd
Brayan Kabore <brayan.kabore@gmail.com>
2025-11-28 21:22:49 -0600

    Gpt help

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './HomePage.jsx'
 import CanvasDashboard from './Dashboard.jsx'
 
 function App() {
